f9f3359db1072f75201920d892b77b5465d7a7ba
dom38 <dominicbyrne38@hotmail.com>
2023-07-18 13:08:36 +0100

    Use global config, add reconciliation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import kopf
 from providers import aws_secrets_manager
-import yaml
-import json
-import boto3
+from config import load_config
+
+# Conditional checks for namespace and annotation
 
 def namespace_inclusion(namespace, memo: kopf.Memo, **_): 
 
@@ -17,13 +17,14 @@ def namespace_inclusion(namespace, memo: kopf.Memo, **_):
 
 def get_annotation(annotations, **_): return annotations.get('dom.dev/distribute') == 'true'
 
+# Setup config loading
+
 @kopf.on.startup()
 def start_background_worker(memo: kopf.Memo, logger, **_):
-    config = yaml.safe_load(open('config/config.yaml'))
-    logger.info(f"config: {json.dumps(config)}")
-    memo.global_config = config
-    global secrets_manager_client 
-    secrets_manager_client = boto3.client("secretsmanager")
+    memo.global_config = load_config()
+
+# Run external secret creation on creation of new k8s secret
+# Only if namespace is whitelisted and/or annotation is present
 
 @kopf.on.create('secret', 
                 when=kopf.any_([
@@ -37,6 +38,9 @@ def create_fn(memo: kopf.Memo, name, body, logger, **_):
     logger.info(f"Result: {result}")
     logger.info(f"Secret: {body}")
 
+# Run external secret creation on update of k8s secret
+# Only if namespace is whitelisted and/or annotation is present
+
 @kopf.on.update('secret', 
                 when=kopf.any_([
                     namespace_inclusion,
@@ -49,3 +53,16 @@ def update_fn(memo: kopf.Memo, name, body, logger, **_):
     logger.info(f"Config: {memo.global_config}")
     logger.info(f"Result: {result}")
     logger.info(f"Secret: {body}")
+
+# Reconcile secrets in cluster
+# Only if namespace is whitelisted and/or annotation is present
+
+@kopf.timer('secret', 
+                when=kopf.any_([
+                    namespace_inclusion,
+                    get_annotation,
+                    ]), 
+            interval=60, 
+            initial_delay=30)
+def check_fn(memo: kopf.Memo, logger, **_):
+    logger.info(f"Starting Reconciliation")
